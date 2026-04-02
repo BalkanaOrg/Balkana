0d87b19351372e2b206cd2f933e60f13bde8bfb7
Simeon Markov <moni300403@abv.bg>
2026-04-02 21:57:34 +0300

    mongodbexportv2

--- tools/Balkana.MongoCoursework/scripts/init-indexes.mongosh.js
+++ tools/Balkana.MongoCoursework/scripts/init-indexes.mongosh.js
@@ -1,14 +1,12 @@
 // Run while connected to the target database, e.g.:
-//   mongosh "mongodb://127.0.0.1:27017/balkana_coursework" scripts/init-indexes.mongosh.js
-// Uses the default `db` from the connection URI.
+//   mongosh "mongodb://127.0.0.1:27017/balkana" scripts/init-indexes.mongosh.js
 
-db.teams.createIndex({ gameId: 1 });
-db.players.createIndex({ nationalityId: 1 });
+db.teams.createIndex({ game: 1 });
+db.players.createIndex({ nationality: 1 });
 db.playerTeamTransfers.createIndex({ playerId: 1 });
 db.playerTeamTransfers.createIndex({ teamId: 1 });
-db.tournaments.createIndex({ gameId: 1 });
-db.tournaments.createIndex({ organizerId: 1 });
-db.tournamentTeams.createIndex({ tournamentId: 1, teamId: 1 }, { unique: true });
-db.tournamentTeams.createIndex({ teamId: 1 });
+db.tournaments.createIndex({ game: 1 });
+db.tournaments.createIndex({ organizer: 1 });
+db.tournaments.createIndex({ "participants.teamId": 1 });
 
 print("Indexes ensured on: " + db.getName());
